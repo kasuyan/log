a8154f4791f82902e4cde1da685ceb3c4ca5b18a
kasuyan <kasuya725@gmail.com>
2020-08-08 23:39:06 +0900

    fix changed template

---
+++ config.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  url: "https://kasuyan.tk",
+  pathPrefix: "/",
+  title: "Blog by Kasuyan",
+  subtitle: "this is my blog.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "",
+  useKatex: false,
+  menu: [
+    {
+      label: "Articles",
+      path: "/",
+    },
+    // {
+    //   label: "About me",
+    //   path: "/pages/about",
+    // },
+    // {
+    //   label: "Contact me",
+    //   path: "/pages/contacts",
+    // },
+  ],
+  author: {
+    name: "Kasuyan",
+    photo: "/photo.jpg",
+    bio: "hoge.",
+    contacts: {
+      email: "",
+      facebook: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "",
+      vkontakte: "",
+      linkedin: "#",
+      instagram: "#",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
+};
---
+++ src/components/Post/Author/Author.js
@@ -0,0 +1,19 @@
+// @flow strict
+import React from "react";
+import { getContactHref } from "../../../utils";
+import styles from "./Author.module.scss";
+import { useSiteMetadata } from "../../../hooks";
+
+const Author = () => {
+  const { author } = useSiteMetadata();
+
+  return (
+    <div className={styles["author"]}>
+      <p className={styles["author__bio"]}>
+        <strong>{author.name}</strong>
+      </p>
+    </div>
+  );
+};
+
+export default Author;
